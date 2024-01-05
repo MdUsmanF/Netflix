0c18a77deb2a805638686865874b9ea8d6e7f56d
Mohammed Usman F <83750506+MdUsmanF@users.noreply.github.com>
2024-01-05 15:15:52 +0000

    CTA,Footer

--- netflix/src/components/CTA.jsx
+++ netflix/src/components/CTA.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <>
+    <div className='flex flex-col items-center justify-center p-24 bg-black'>
+      <h4 className='pb-4 text-2xl text-white'>Ready to watch? Enter your email to create or restart your membership.</h4>
+      <div className='flex items-center justify-center w-full'>
+        <input type="email" placeholder='Enter Your Email' className='w-2/6 py-4 px-3 mr-2 border border-gray-800 rounded-md' />
+        <button className='w-1/6 py-4 px-4 bg-red-600 text-white rounded-md'>Get Started</button>
+      </div>
+    </div>
+    <hr class="h-3 bg-gray-800 border-0 dark:bg-gray-800"></hr>
+    </>
   )
 }
 
--- netflix/src/components/Footer.jsx
+++ netflix/src/components/Footer.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='text-slate-400 text-grey text-sm grid lg:grid-cols-4 md:grid-cols-2 gap-8 p-24 bg-black'>
+    <div className='grid grid-rows-1 gap-2'>
+        <u>FAQ's</u>
+        <u>Investor Relations</u>
+        <u>Privacy</u>
+        <u>Speed Test</u>
+    </div>
+    <div className='grid grid-rows-1 gap-2'>
+        <u>Help Center</u>
+        <u>Jobs</u>
+        <u>Cookiee Preference</u>
+        <u>Legal Notice</u>
+    </div>
+    <div className='grid grid-rows-1 gap-2'>
+        <u>Account</u>
+        <u>Ways to Watch</u>
+        <u>Corporate Information</u>
+        <u>Only on Netflix</u>
+    </div>
+    <div className='grid grid-rows-1 gap-2'>
+        <u>Media Center</u>
+        <u>Terms of Use</u>
+        <u>Contact Us</u>
+        <u>Refund Policy</u>
+    </div>
+    </div>
   )
 }
 
