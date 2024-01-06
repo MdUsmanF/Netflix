19c8fe191f69d03ef1cacc3568b1facfdd64ddab
Mohammed Usman F <83750506+MdUsmanF@users.noreply.github.com>
2024-01-06 09:18:59 +0000

    Bento Box Implementation

--- netflix/src/App.jsx
+++ netflix/src/App.jsx
@@ -4,11 +4,13 @@ import FAQ from './components/FAQ';
 import Footer from './components/Footer';
 import HeroHeader from './components/HeroHeader';
 import "./components/Navbar"
+import Features from './components/Features';
 
 function App() {
   return (
     <div>
       <HeroHeader/>
+      <Features/>
       <FAQ/>
       <CTA/>
       <Footer/>
