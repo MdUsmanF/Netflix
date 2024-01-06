d4e7183b5cfb26f5f9375e7c4d049b7e8b686a54
Mohammed Usman F <83750506+MdUsmanF@users.noreply.github.com>
2024-01-06 06:45:23 +0000

    Responsive

--- netflix/src/components/CTA.jsx
+++ netflix/src/components/CTA.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const CTA = () => {
   return (
     <>
-    <div className='flex flex-col items-center justify-center p-24 bg-black'>
+    <div className='flex flex-col items-center justify-center p-4 md:p-24 bg-black'>
       <h4 className='pb-4 text-2xl text-white'>Ready to watch? Enter your email to create or restart your membership.</h4>
-      <div className='flex items-center justify-center w-full'>
-        <input type="email" placeholder='Enter Your Email' className='w-2/6 py-4 px-3 mr-2 border border-gray-800 rounded-md' />
-        <button className='w-1/6 py-4 px-4 bg-red-600 text-white rounded-md'>Get Started</button>
+      <div className='flex flex-col md:flex-row items-center justify-center w-full'>
+        <input type="email" placeholder='Enter Your Email' className='w-full md:w-2/6 py-4 px-3 mb-2 md:mb-0 md:mr-2 border border-gray-800 rounded-md' />
+        <button className='w-full md:w-1/6 py-4 px-4 bg-red-600 text-white rounded-md'>Get Started</button>
       </div>
     </div>
-    <hr class="h-3 bg-gray-800 border-0 dark:bg-gray-800"></hr>
+    <hr class="h-2 bg-gray-800 border-0 dark:bg-gray-800"></hr>
     </>
   )
 }
